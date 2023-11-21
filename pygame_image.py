920a39c32a61ec46886230616c98d4982abc1d55
sugimoto <c0b22079b2@edu.teu.ac.jp>
2023-11-21 15:59:27 +0900

    renshu1

--- pygame_image.py
+++ pygame_image.py
@@ -4,19 +4,21 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     ch_img = pg.image.load("ex01/fig/3.png")
     ch_img = pg.transform.flip(ch_img, True, False)
     tmr = 0
+    x=0
     ch_images=[ch_img,pg.transform.rotate(ch_img,10)]
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(ch_images[1], [500, 500])
+        x=tmr%1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(ch_images[tmr%2], [300, 300])
+        print(tmr)
         pg.display.update()
         tmr += 1        
         clock.tick(10)
