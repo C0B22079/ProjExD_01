57a22b1dfc941ca0b06a051bdd01b87e00b9c085
sugimoto <c0b22079b2@edu.teu.ac.jp>
2023-11-21 17:04:32 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -16,10 +16,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr%1600
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1500-x, 0])
-        screen.blit(pg.transform.rotate(ch_img,d[tmr%20]), [300, 300])
+        screen.blit(pg.transform.flip(bg_img, True, False), [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(pg.transform.rotate(ch_img,d[tmr%20]), [300, 200])
         print(-x)
         pg.display.update()
         tmr += 1        
