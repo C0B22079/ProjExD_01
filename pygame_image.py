1a5a838092d4ae803680c27c5845afc8b6286d0b
sugimoto <c0b22079b2@edu.teu.ac.jp>
2023-11-21 15:33:33 +0900

    renshuuu

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((1600, 900))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    ch_img = pg.image.load("ex01/fig/3.png")
+    ch_img = pg.transform.flip(ch_img, True, False)
+    ch_img = pg.transform.rotate(ch_img,10)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(ch_img, [200, 100])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
