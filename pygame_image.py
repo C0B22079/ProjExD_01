20ed7eb55cc7f01746ad1a7788e1f5d74fb60024
sugimoto <c0b22079b2@edu.teu.ac.jp>
2023-11-21 15:40:25 +0900

    renshu0

--- pygame_image.py
+++ pygame_image.py
@@ -9,14 +9,14 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     ch_img = pg.image.load("ex01/fig/3.png")
     ch_img = pg.transform.flip(ch_img, True, False)
-    ch_img = pg.transform.rotate(ch_img,10)
     tmr = 0
+    ch_images=[ch_img,pg.transform.rotate(ch_img,10)]
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(ch_img, [200, 100])
+        screen.blit(ch_images[1], [500, 500])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
